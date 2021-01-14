d3488555793c62cec4f15ef9302c22d51d351582
Mathijsla <38563734+Mathijsla@users.noreply.github.com>
2021-01-14 18:14:11 +0100

    First UI elements added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,20 +2,31 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
 
-public class Main extends Application {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main extends Application implements Initializable {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1920, 1080));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
